54c0d94a7ca2815ed47d2604454004c255076c4a
Codenetz <codenetz.mail@gmail.com>
2018-04-19 23:36:43 +0300

    Add request type checking on validation middleware

--- src/server/modules/example/routing/routes.js
+++ src/server/modules/example/routing/routes.js
@@ -10,11 +10,16 @@ module.exports = (app) => {
   /** Routes */
   app.get("/example", itemController.listAction);
 
-  app.post("/example", validation.bind(null, Joi.object().keys({
-    username: Joi.string().alphanum().min(3).max(30).required(),
-    password: Joi.string().regex(/^[a-zA-Z0-9]{3,30}$/).required()
-  })),
-  itemController.listAction);
+  app.post(
+    "/example",
+    validation.bind(
+      null,
+      Joi.object().keys({
+        name: Joi.string().required()
+      }),
+      "body"
+    ),
+    itemController.listAction);
 
   app.post('/example-image',
     app.get("multer").single('image'),
